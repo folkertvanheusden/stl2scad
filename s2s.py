49ff32e070210d8ab714e4f05045e8cd241decb2
Folkert van Heusden <folkert@vanheusden.com>
2026-04-20 16:27:30 +0200

    1st version

---
+++ s2s.py
@@ -0,0 +1,24 @@
+#! /usr/bin/env python3
+
+from stl import mesh
+import numpy
+import sys
+
+def v2s(v):
+    return '[' + (','.join([str(V) for V in v])) + ']'
+
+m = mesh.Mesh.from_file(sys.argv[1])
+for record in m.data:
+    triangle = record[1]
+
+    print('''
+polyhedron(
+    points = [''')
+    print(v2s(triangle[0]) + ',')
+    print(v2s(triangle[1]) + ',')
+    print(v2s(triangle[2]))
+    print('''],
+    faces = [
+        [0, 1, 2]
+    ]
+);''')
